e39c473647a94b36c1bc72faaaf1965e7e0cd281
Keagan Dickinson <keagantdickinson@gmail.com>
2022-06-19 21:37:23 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let button1 = document.getElementById('button-1');
+let button2 = document.getElementById('button-2');
+let button3 = document.getElementById('button-3');
+let button4 = document.getElementById('button-4');
+let button5 = document.getElementById('button-5');
+
+button1.addEventListener('click', clickfunction)
+
+
+
+
+function clickfunction (e) {
+    ${e.target.}.style.backgroundColor = 'orange';
+}
